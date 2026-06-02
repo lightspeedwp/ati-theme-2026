81571bda5f26696f0f485d5929ff00c21e11933d
Zared Rogers <zared@lightspeedwp.agency>
2026-06-02 12:08:59 +0200

    Updating mega menu card pattern

--- patterns/ati-destination-link-card.php
+++ patterns/ati-destination-link-card.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Title: ATI Destination Link Card
- * Slug: ati-destination-link-card
+ * Title: ATI Mega Menu Card
+ * Slug: ati-mega-menu-card
  * Theme: ati-theme-2026
  * Description: Linked destination teaser card with image and centred heading.
  * Inserter: yes
  */
 ?>
 
-<!-- wp:group {"metadata":{"name":"Destination Link Card","patternName":"ati-destination-link-card"},"className":"is-style-destination-link-card-dark","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"backgroundColor":"primary-500","textColor":"base","layout":{"type":"constrained"},"href":"/destination/zimbabwe/","linkDestination":"custom","ariaLabel":"Zimbabwe destination card"} -->
+<!-- wp:group {"metadata":{"name":"Mega Menu Card","patternName":"ati-mega-menu-card"},"className":"is-style-destination-link-card-dark","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"backgroundColor":"primary-500","textColor":"base","layout":{"type":"constrained"},"href":"/destination/zimbabwe/","linkDestination":"custom","ariaLabel":"Zimbabwe destination card"} -->
 <div aria-label="Zimbabwe destination card" class="wp-block-group is-style-destination-link-card-dark has-base-color has-primary-500-background-color has-text-color has-background has-link-color"><!-- wp:image {"id":18580,"aspectRatio":"3/2","scale":"cover","sizeSlug":"large","linkDestination":"none"} -->
 <figure class="wp-block-image size-large"><img src="https://ati-holidays.lightspeedwp.dev/wp-content/uploads/2022/09/Zimbabwe-Menu-Final-1024x618.jpg" alt="Zimbabwe landscape" class="wp-image-18580" style="aspect-ratio:3/2;object-fit:cover"/></figure>
 <!-- /wp:image -->
