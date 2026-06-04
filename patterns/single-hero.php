1ed064d6dc41d4713160dccfb5d043a92ad2d7f9
Zared Rogers <zared@lightspeedwp.agency>
2026-06-04 13:55:33 +0200

    Creating single hero pattern

---
+++ patterns/single-hero.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Title: Single Hero
+ * Slug: ati-single-hero
+ * Theme: ati-theme-2026
+ * Inserter: yes
+ */
+?>
+
+
+
+<!-- wp:group {"metadata":{"name":"Single Hero"},"align":"full","style":{"spacing":{"margin":{"top":"var:preset|spacing|0","bottom":"var:preset|spacing|0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="margin-top:var(--wp--preset--spacing--0);margin-bottom:var(--wp--preset--spacing--0)"><!-- wp:cover {"url":"https://ati-holidays.lightspeedwp.dev/wp-content/uploads/2023/02/Hoarsib-valley-1024x684.jpg","id":19822,"dimRatio":10,"overlayColor":"contrast","isUserOverlayColor":true,"minHeight":50,"contentPosition":"center center","isDark":false,"tagName":"section","sizeSlug":"large","metadata":{"name":"Hero"},"className":"alignfull is-style-default","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|0","bottom":"var:preset|spacing|80","right":"var:preset|spacing|0","left":"var:preset|spacing|0"},"blockGap":"var:preset|spacing|70"}},"layout":{"type":"constrained"}} -->
+<section class="wp-block-cover is-light alignfull is-style-default" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--0);padding-right:var(--wp--preset--spacing--0);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--0);min-height:50px"><img class="wp-block-cover__image-background wp-image-19822 size-large" alt="" src="https://ati-holidays.lightspeedwp.dev/wp-content/uploads/2023/02/Hoarsib-valley-1024x684.jpg" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-contrast-background-color has-background-dim-10 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"metadata":{"name":"Breadcrumbs"},"align":"full","className":"has-text-color has-link-color is-style-default","style":{"elements":{"link":{":hover":{"color":{"text":"var:preset|color|neutral-900"}}}},"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"},"margin":{"top":"0","bottom":"0"}},"color":{"background":"#dbbd8dde"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-text-color has-link-color is-style-default has-background" style="background-color:#dbbd8dde;margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20)"><!-- wp:group {"align":"wide","className":"is-style-default","fontSize":"200","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide is-style-default has-200-font-size"><!-- wp:yoast-seo/breadcrumbs /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"wide","style":{"dimensions":{"minHeight":"0px"},"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|40","right":"var:preset|spacing|20","left":"var:preset|spacing|20"}}},"layout":{"type":"constrained","justifyContent":"center","contentSize":"1100px"}} -->
+<div class="wp-block-group alignwide" style="min-height:0px;padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--20)"><!-- wp:post-title {"textAlign":"center","level":1,"align":"wide","className":"is-style-shadow-text","style":{"typography":{"fontStyle":"normal","fontWeight":"500","textAlign":"center"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontSize":"800"} /-->
+
+<!-- wp:paragraph {"metadata":{"name":"Tagline","bindings":{"content":{"source":"lsx/post-meta","args":{"key":"tagline"}}}},"className":"lsx-tagline-wrapper is-style-shadow-text has-base-color has-text-color has-link-color","style":{"typography":{"textAlign":"center"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base"} -->
+<p class="has-text-align-center lsx-tagline-wrapper is-style-shadow-text has-base-color has-text-color has-link-color"></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></section>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
